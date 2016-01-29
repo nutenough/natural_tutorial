a53352ca446d4723f7e20e131da70b7e5c7fd88a
Christian Weber <weber.chris@hotmail.de>
2016-01-29 12:06:48 +0100

    Added: tf-idf changes

--- tf_idf_solution.js
+++ tf_idf_solution.js
@@ -6,6 +6,7 @@ tfidf.addDocument('this document is about javascript.');
 tfidf.addDocument('this document is about javascript and texttechnology.');
 tfidf.addDocument('this document is about texttechnology. it has examples for the course texttechnology');
 
+// Auch measure mehrerer Terme möglich
 console.log('\nWort \"texttechnology\"');
 tfidf.tfidfs('texttechnology', function(i, measure) {
     console.log('document #' + i + ' is ' + measure);
@@ -13,5 +14,14 @@ tfidf.tfidfs('texttechnology', function(i, measure) {
 
 console.log('\nWort \"javascript\"');
 tfidf.tfidfs('javascript', function(i, measure) {
+    console.log('document #' + i + ' is ' + measure);
+});
+
+// Einlesen von Dokumenten
+tfidf.addFileSync('test_1.txt');
+tfidf.addFileSync('test_2.txt');
+
+console.log('\nWort \"javascript\"');
+tfidf.tfidfs('texttechnology', function(i, measure) {
     console.log('document #' + i + ' is ' + measure);
 });
